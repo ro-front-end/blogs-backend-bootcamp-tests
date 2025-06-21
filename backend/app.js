c90c2b5368b82d4d711e8e16e4be6ab5258f9480
ro-front-end <rockfrontend@outlook.com>
2025-06-20 20:50:04 -0600

    change name for vercel deploy

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://techy-blogs-fullstack.onrender.com",
+      "https://techy-blogs-fullstack.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
